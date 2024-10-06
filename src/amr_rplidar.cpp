50bac03ed438571c930c11f68c7a1d68d50e36b7
XKHoshizora <hoshizoranihon@gmail.com>
2024-10-07 04:41:55 +0900

    Update

--- src/amr_rplidar.cpp
+++ src/amr_rplidar.cpp
@@ -26,19 +26,16 @@ public:
         // 更新 frame_id
         scan_out.header.frame_id = frame_id_;
 
-        // 添加 180 度（π 弧度）的补偿
-        double compensation = M_PI;
-
         // 调整角度范围和增量
-        scan_out.angle_min = -scan_in->angle_max + angle_offset_ + compensation;
-        scan_out.angle_max = -scan_in->angle_min + angle_offset_ + compensation;
+        scan_out.angle_min = -scan_in->angle_max + angle_offset_;
+        scan_out.angle_max = -scan_in->angle_min + angle_offset_;
         scan_out.angle_increment = -scan_in->angle_increment;
 
-        // 反转数据数组
-        std::reverse(scan_out.ranges.begin(), scan_out.ranges.end());
-        if (!scan_out.intensities.empty()) {
-            std::reverse(scan_out.intensities.begin(), scan_out.intensities.end());
-        }
+        // 确保角度在 [-π, π] 范围内
+        while (scan_out.angle_min > M_PI) scan_out.angle_min -= 2*M_PI;
+        while (scan_out.angle_min < -M_PI) scan_out.angle_min += 2*M_PI;
+        while (scan_out.angle_max > M_PI) scan_out.angle_max -= 2*M_PI;
+        while (scan_out.angle_max < -M_PI) scan_out.angle_max += 2*M_PI;
 
         // 发布转换后的数据
         scan_pub_.publish(scan_out);
